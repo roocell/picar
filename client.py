4f4b753fd3a3f86c0665a398c2c4af0647722934
Michael Russell <roocell@gmail.com>
2020-05-07 05:06:36 +0100

    location updates

--- client.py
+++ client.py
@@ -8,16 +8,26 @@ from importlib import import_module
 import logging
 from threading import Thread
 
+# we want to run a flask app here as well to serve up the location page
+# this app will get the location from a cellphone and then emit that info
+# to the server
+from flask import Flask, jsonify, render_template, request, send_from_directory
+import json
+import apikeys
+import flask_socketio
+
+# things needed to control raspi I/O
+import atexit
+
 # to suppress InsecureRequestWarning when using https
 import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 os.environ['CAMERA'] = "opencv"
 os.environ['OPENCV_CAMERA_SOURCE'] = "0"
-os.environ['FPS'] = "60"
+os.environ['FPS'] = "1"
 fps = int(os.environ['FPS'])
 URL = "https://www.roocell.com:5000"
-hb_time = 0
 
 # import camera driver
 if os.environ.get('CAMERA'):
@@ -36,8 +46,38 @@ log.addHandler(ch)
 
 sio = socketio.Client(engineio_logger=False, logger=False, ssl_verify=False)
 
-camera_thread = None
+# instantiate app and flask socketio
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+fsio = flask_socketio.SocketIO(app)
 
+#=============================================================
+# location routes w/ socketio
+@app.route('/location')
+def location():
+    return render_template('location.html', key = apikeys.google_map_api)
+@fsio.on('update_location', namespace='/updatelocation')
+def updatelocation(message):
+    print("======================================")
+    print("location received, forwarding to server")
+    # TODO: json.loads doesnt' get all the decimal places.
+    data = json.loads(message['data'])
+    print(data)
+    loc = {"latitude":data['latitude'], "longitude":data['longitude']}
+    # emit back to web client so it can update location on it's page
+    fsio.emit('location_updated', loc, namespace="/updatelocation")
+    # relay location to server
+    sio.emit('location_updated', loc, namespace="/serverupdatelocation")
+    return "OK"
+
+@fsio.on('connect', namespace='/updatelocation')
+def connect():
+    print("flask client connected")
+    return "OK"
+
+# ============================================
+# Camera events
+camera_thread = None
 @sio.event
 def connect():
     global camera_thread
@@ -102,25 +142,50 @@ def gen(camera):
     if (sio.connected == False):
         log.debug("stopped sending frames")
 
-log.debug("entering main loop")
-while 1:
-    while sio.connected == False:
-        try:
-            print("connecting... ")
-            sio.connect(URL, namespaces=['/heartbeat', '/video'])
-        except:
-            print("ERROR: connection refused: check your URL, server running, port forwarding, internet connection, etc")
-        time.sleep(3) # wait a few seconds for the connection to establish
 
-    # see if we need to start the camera thread
-    print(isinstance(camera_thread, Thread))
-    if (isinstance(camera_thread, Thread)):
-        if (camera_thread.is_alive() == False):
-            log.debug('kicking off camera thread')
-            camera_thread.start()
 
-    if (sio.connected == True):
-        print("ML: sending hb_from_client")
-        hb_time = time.time()
-        sio.emit("hb_from_client", {'foo' : 'bar'}, namespace='/heartbeat', callback=hb_response)
-        time.sleep(10)
+#=======================================================
+# main loop
+hb_time = 0
+def main_loop(arg):
+    global hb_time
+    global sio
+    global camera_thread
+    log.debug("entering main loop")
+    while 1:
+        while sio.connected == False:
+            try:
+                print("connecting... ")
+                sio.connect(URL, namespaces=['/heartbeat', '/video', '/serverupdatelocation'])
+            except:
+                print("ERROR: connection refused: check your URL, server running, port forwarding, internet connection, etc")
+            time.sleep(3) # wait a few seconds for the connection to establish
+
+        # see if we need to start the camera thread
+        if (isinstance(camera_thread, Thread)):
+            if (camera_thread.is_alive() == False):
+                log.debug('kicking off camera thread')
+                camera_thread.start()
+
+        if (sio.connected == True):
+            print("ML: sending hb_from_client")
+            hb_time = time.time()
+            sio.emit("hb_from_client", {'foo' : 'bar'}, namespace='/heartbeat', callback=hb_response)
+            time.sleep(10)
+# end of main_loop
+
+
+#=====================================================
+def cleanup():
+    print("cleaning up")
+    #picar.destroy()
+
+if __name__ == '__main__':
+    atexit.register(cleanup)
+
+    # kick off main loop as a thread - because we're a flask app
+    ml = Thread(target=main_loop, args=(False,))
+    ml.start()
+
+    # need to use self-signed certs because we don't have a domain
+    fsio.run(app, certfile='cert.pem', keyfile='key.pem', debug=True, host='0.0.0.0')
